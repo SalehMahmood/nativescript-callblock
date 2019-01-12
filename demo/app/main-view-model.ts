ac7cf9726ec1ac518ee527150d2b7262bb1c6b0f
Saleh Mahmood <thesalehmahmood@gmail.com>
2019-01-13 00:19:50 +0500

    Succussfully integrated and exported packages from nativescript_callblock.aar.

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,13 +1,19 @@
 import { Observable } from 'tns-core-modules/data/observable';
 import { Callblock } from 'nativescript-callblock';
 
+declare class com {
+  public static salehmahmood;
+}
+
+declare class CallBlocker {
+}
 export class HelloWorldModel extends Observable {
   public message: string;
   private callblock: Callblock;
 
   constructor() {
     super();
-
+    // console.log("Obj", com.salehmahmood.nativescriptcallblock.CallBlocker);
     this.callblock = new Callblock();
     this.message = this.callblock.message;
   }
