6fcd590f72707b642dc57cc37d54fbf7fb09c911
Saleh Mahmood <thesalehmahmood@gmail.com>
2019-01-13 21:28:10 +0500

    demo/App.ts cleaning.

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,7 +1,3 @@
 ﻿import * as application from 'tns-core-modules/application';
-import { CallBlock } from 'nativescript-callblock';
 
 application.start({ moduleName: "main-page" });
-
-console.log("--- CallBLocker ---")
-// console.dir(new CallBlock());
